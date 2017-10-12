40e59f3a6cd33676e9e580a0647a42750652d35c
pengfuyuzhen <pfyz2011@gmail.com>
2017-10-12 19:35:27 -0400

    Created Xcode project

---
+++ InQwire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InQwire
+//
+//  Created by Fuyuzhen Peng on 10/12/17.
+//  Copyright © 2017 Fuyuzhen Peng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
